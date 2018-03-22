ff22dfa1e5ffa8559f40ccb5207fd2db06a41cf4
Crehul Vlad <34303919+VladC12@users.noreply.github.com>
2018-03-22 16:12:47 +0200

    Update Omni-Holder0.5.ino

--- Omni-Holder0.5/Omni-Holder0.5/Omni-Holder0.5.ino
+++ Omni-Holder0.5/Omni-Holder0.5/Omni-Holder0.5.ino
@@ -1,204 +1,223 @@
-
-
+#include <Wire.h>
+#include <Kalman.h> // Source: https://github.com/TKJElectronics/KalmanFilter
 #include <Servo.h>
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-#include "MPU6050.h" // not necessary if using MotionApps include file
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
+#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http ://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+	  Kalman kalmanX; // Create the Kalman instances
+	  Kalman kalmanY;
+	  Servo Servo1; // First Servo off the chassis
+	  Servo Servo2; // Second Servo off the chassis
+	  int Servo1Pos = 0;
+	  int Servo2Pos = 0;
+	  const uint8_t IMUAddress = 0x68; // AD0 is logic low on the PCB
+	  const uint16_t I2C_TIMEOUT = 1000; // Used to check for errors in I2C communication
+	  uint8_t i2cWrite(uint8_t registerAddress, uint8_t data, bool sendStop) {
+		  return i2cWrite(registerAddress, &data, 1, sendStop); // Returns 0 on success
+	  }
+	  uint8_t i2cWrite(uint8_t registerAddress, uint8_t *data, uint8_t length, bool sendStop) {
+		  Wire.beginTransmission(IMUAddress);
+		  Wire.write(registerAddress);
+		  Wire.write(data, length);
+		  uint8_t rcode = Wire.endTransmission(sendStop); // Returns 0 on success
+		  if (rcode) {
+			  Serial.print(F("i2cWrite failed: "));
+			  Serial.println(rcode);
+		  }
+		  return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
+	  }
+	  uint8_t i2cRead(uint8_t registerAddress, uint8_t *data, uint8_t nbytes) {
+		  uint32_t timeOutTimer;
+		  Wire.beginTransmission(IMUAddress);
+		  Wire.write(registerAddress);
+		  uint8_t rcode = Wire.endTransmission(false); // Don't release the bus
+		  if (rcode) {
+			  Serial.print(F("i2cRead failed: "));
+			  Serial.println(rcode);
+			  return rcode; // See: http://arduino.cc/en/Reference/WireEndTransmission
+		  }
+		  Wire.requestFrom(IMUAddress, nbytes, (uint8_t)true); // Send a repeated start and then release the bus after reading
+			  for (uint8_t i = 0; i < nbytes; i++) {
+				  if (Wire.available())
+					  data[i] = Wire.read();
+				  else {
+					  timeOutTimer = micros();
+					  while (((micros() - timeOutTimer) < I2C_TIMEOUT) && !Wire.available());
+					  if (Wire.available())
+						  data[i] = Wire.read();
+					  else {
+						  Serial.println(F("i2cRead timeout"));
+						  return 5; // This error value is not already taken by endTransmission
+					  }
+				  }
+			  }
+		  return 0; // Success
+	  }
+	  /* IMU Data */
+	  double accX, accY, accZ;
+	  double gyroX, gyroY, gyroZ;
+	  int16_t tempRaw;
+	  double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+	  double compAngleX, compAngleY; // Calculated angle using a complementary filter
+	  double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+	  uint32_t timer;
+	  uint8_t i2cData[14]; // Buffer for I2C data
+						   // TODO: Make calibration routine
+	  void setup() {
+		  Serial.begin(115200);
+		  Wire.begin();
+#if ARDUINO >= 157
+		  Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+#else
+		  TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
 #endif
+		  Servo1.attach(10); // attaches the servo on D11 to the servo object
+		  Servo2.attach(11); // Second servo on D11
+		  delay(50);
+		  Servo1.write(0); // These are command checks to see if the servos work and
+		  Servo2.write(60); // to help w/ the initial installation.
+		  delay(500); // Make sure these movements are clear from the rest of the chassis.
+		  Servo1.write(180);
+		  Servo2.write(120);
+		  delay(500);
+		  Servo1.write(0);
+		  Servo2.write(90);
+		  delay(500);
 
-#define LED_PIN 13
-bool blinkState = true;
-
-Servo Servo1;   // First Servo off the chassis
-Servo Servo2;   // Second Servo off the chassis
-
-int Servo1Pos = 0;
-int Servo2Pos = 0;
-
-float mpuPitch = 0;
-float mpuRoll = 0;
-float mpuYaw = 0;
-
-
-// define MPU instance
-MPU6050 mpu;                    // class default I2C address is 0x68; specific I2C addresses may be passed as a parameter here
-
-								// MPU control/status vars
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-						// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-
-						// relative ypr[x] usage based on sensor orientation when mounted, e.g. ypr[PITCH]
-#define PITCH   1     // defines the position within ypr[x] variable for PITCH; may vary due to sensor orientation when mounted
-#define ROLL  2     // defines the position within ypr[x] variable for ROLL; may vary due to sensor orientation when mounted
-#define YAW   0     // defines the position within ypr[x] variable for YAW; may vary due to sensor orientation when mounted
-
-						// ================================================================
-						// ===                      INITIAL SETUP                       ===
-						// ================================================================
-
-void setup()
-{
-
-	Servo1.attach(10);  // attaches the servo on D11 to the servo object
-	Servo2.attach(11);  // Second servo on D11
-	delay(50);
-	Servo1.write(0);  // These are command checks to see if the servos work and
-	Servo2.write(60); // to help w/ the initial installation.
-	delay(500);   // Make sure these movements are clear from the rest of the chassis.
-	Servo1.write(180);
-	Servo2.write(120);
-	delay(500);
-	Servo1.write(0);
-	Servo2.write(90);
-	delay(500);
-
-	// join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-	Wire.begin();
-	TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-	Fastwire::setup(400, true);
+		  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+		  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+		  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+		  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+		  while (i2cWrite(0x19, i2cData, 4, false)); // Write to all four registers at once
+		  while (i2cWrite(0x6B, 0x01, true)); // PLL with X axis gyroscope reference and disable sleep mode
+		  while (i2cRead(0x75, i2cData, 1));
+		  if (i2cData[0] != 0x68) { // Read "WHO_AM_I" register
+			  Serial.print(F("Error reading sensor"));
+			  while (1);
+		  }
+		  delay(100); // Wait for sensor to stabilize
+					  /* Set kalman and gyro starting angle */
+		  while (i2cRead(0x3B, i2cData, 6));
+		  double last_accX = accX, last_accY = accY, last_accZ = accZ;
+		  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+		  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+		  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+		  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+		  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+		  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+		  double roll = atan2(accY, accZ) * RAD_TO_DEG;
+		  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+		  double roll = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+		  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
 #endif
+		  kalmanX.setAngle(roll); // Set starting angle
+		  kalmanY.setAngle(pitch);
+		  gyroXangle = roll;
+		  gyroYangle = pitch;
+		  compAngleX = roll;
+		  compAngleY = pitch;
+		  timer = micros();
+	  }
+	  void loop() {
+		  /* Update all the values */
+		  while (i2cRead(0x3B, i2cData, 14));
+		  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+		  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+		  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+		  tempRaw = (int16_t)((i2cData[6] << 8) | i2cData[7]);
+		  gyroX = (int16_t)((i2cData[8] << 8) | i2cData[9]);
+		  gyroY = (int16_t)((i2cData[10] << 8) | i2cData[11]);
+		  gyroZ = (int16_t)((i2cData[12] << 8) | i2cData[13]);;
+		  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+		  timer = micros();
+		  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+		  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+		  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+		  double roll = atan2(accY, accZ) * RAD_TO_DEG;
+		  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+		  double roll = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+		  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+		  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+		  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+#ifdef RESTRICT_PITCH
+											// This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+			  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+				  kalmanX.setAngle(roll);
+				  compAngleX = roll;
+				  kalAngleX = roll;
+				  gyroXangle = roll;
+			  }
+			  else
+				  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+		  if (abs(kalAngleX) > 90)
+			  gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+		  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+#else
+											// This fixes the transition problem when the accelerometer angle jumps between -180 and 180
+		  degrees
+			  if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+				  kalmanY.setAngle(pitch);
+				  compAngleY = pitch;
+				  kalAngleY = pitch;
+				  gyroYangle = pitch;
+			  }
+			  else
+				  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+		  if (abs(kalAngleY) > 90)
+			  gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+		  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+#endif
+		  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+		  gyroYangle += gyroYrate * dt;
+		  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+		  //gyroYangle += kalmanY.getRate() * dt;
+		  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+			  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+		  // Reset the gyro angle when it has drifted too much
+		  if (gyroXangle < -180 || gyroXangle > 180)
+			  gyroXangle = kalAngleX;
+		  if (gyroYangle < -180 || gyroYangle > 180)
+			  gyroYangle = kalAngleY;
+		  if (accX > 0) {
+			  Servo1.write(-compAngleY + 90 -accX/274);
+		  }
+		  else {
+			  Servo1.write(-compAngleY + 90);
+		  }
+		  Servo2.write(compAngleX + 90);
 
-	Serial.begin(115200);
-	while (!Serial);      // wait for Leonardo enumeration, others continue immediately
-
-						  // initialize device
-	Serial.println(F("Initializing I2C devices..."));
-	mpu.initialize();
-
-	// verify connection
-	Serial.println(F("Testing device connections..."));
-	Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-	// load and configure the DMP
-	Serial.println(F("Initializing DMP"));
-	devStatus = mpu.dmpInitialize();
 
 
-	// INPUT CALIBRATED OFFSETS HERE; SPECIFIC FOR EACH UNIT AND EACH MOUNTING CONFIGURATION!!!!
+		  
 
-	mpu.setXGyroOffset(118);
-	mpu.setYGyroOffset(-44);
-	mpu.setZGyroOffset(337);
-	mpu.setXAccelOffset(-651);
-	mpu.setYAccelOffset(670);
-	mpu.setZAccelOffset(1895);
+		  /* Print Data */
+#if 1 // Set to 1 to activate
+		  Serial.print(accX); Serial.print("\t");
+		 // Serial.print(accY); Serial.print("\t");
+		 // Serial.print(accZ); Serial.print("\t");
+		 // Serial.print(gyroX); Serial.print("\t");
+		 // Serial.print(gyroY); Serial.print("\t");
+		 // Serial.print(gyroZ); Serial.print("\t");
+		  Serial.print("\t");
+#endif
+		 /* Serial.print(roll); Serial.print("\t");
+		  Serial.print(gyroXangle); Serial.print("\t");
+		  Serial.print(compAngleX); Serial.print("\t");
+		  Serial.print(kalAngleX); Serial.print("\t");
+		  Serial.print("\t");
+		  Serial.print(pitch); Serial.print("\t");
+		  Serial.print(gyroYangle); Serial.print("\t");
+		  Serial.print(compAngleY); Serial.print("\t");
+		  Serial.print(kalAngleY); Serial.print("\t");*/
 
-	// make sure it worked (returns 0 if so)
-	if (devStatus == 0)
-	{
-		// turn on the DMP, now that it's ready
-		Serial.println(F("Enabling DMP"));
-		mpu.setDMPEnabled(true);
-
-		// enable Arduino interrupt detection
-		Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)"));
-		mpuIntStatus = mpu.getIntStatus();
-
-		// get expected DMP packet size for later comparison
-		packetSize = mpu.dmpGetFIFOPacketSize();
-	}
-	else
-	{
-		// ERROR!
-		// 1 = initial memory load failed, 2 = DMP configuration updates failed (if it's going to break, usually the code will be 1)
-		Serial.print(F("DMP Initialization failed code = "));
-		Serial.println(devStatus);
-	}
-
-	// configure LED for output
-	pinMode(LED_PIN, OUTPUT);
-
-} // setup()
-
-
-
-  // ================================================================
-  // ===                    MAIN PROGRAM LOOP                     ===
-  // ================================================================
-
-void loop(void)
-{
-	processAccelGyro();
-}   // loop()
-
-
-
-	// ================================================================
-	// ===                    PROCESS ACCEL/GYRO IF AVAILABLE       ===
-	// ================================================================
-
-void processAccelGyro()
-{
-
-	// Get INT_STATUS byte
-	mpuIntStatus = mpu.getIntStatus();
-
-	// get current FIFO count
-	fifoCount = mpu.getFIFOCount();
-
-	// check for overflow (this should never happen unless our code is too inefficient)
-	if ((mpuIntStatus & 0x10) || fifoCount == 1024)
-	{
-		// reset so we can continue cleanly
-		mpu.resetFIFO();
-		Serial.println(F("FIFO overflow!"));
-		return;
-	}
-
-	if (mpuIntStatus & 0x02)  // otherwise continue processing
-	{
-		// check for correct available data length
-		if (fifoCount < packetSize)
-			return; //  fifoCount = mpu.getFIFOCount();
-
-					// read a packet from FIFO
-		mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-		// track FIFO count here in case there is > 1 packet available
-		fifoCount -= packetSize;
-
-		// flush buffer to prevent overflow
-		mpu.resetFIFO();
-
-		// display Euler angles in degrees
-		mpu.dmpGetQuaternion(&q, fifoBuffer);
-		mpu.dmpGetGravity(&gravity, &q);
-		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-		mpuPitch = ypr[PITCH] * 180 / M_PI;
-		mpuRoll = ypr[ROLL] * 180 / M_PI;
-		mpuYaw = ypr[YAW] * 180 / M_PI;
-
-		// flush buffer to prevent overflow
-		mpu.resetFIFO();
-
-		// blink LED to indicate activity
-		blinkState = !blinkState;
-		digitalWrite(LED_PIN, blinkState);
-
-		// flush buffer to prevent overflow
-		mpu.resetFIFO();
-
-		Servo1.write(-mpuPitch + 90);
-		Servo2.write(mpuRoll + 90);
-		//delay(10);
-
-		// flush buffer to prevent overflow
-		mpu.resetFIFO();
-
-	} // if (mpuIntStatus & 0x02)
-}  // processAccelGyro()
+#if 0 // Set to 1 to print the temperature
+		  Serial.print("\t");
+		  double temperature = (double)tempRaw / 340.0 + 36.53;
+		  Serial.print(temperature); Serial.print("\t");
+#endif
+		  Serial.print("\r\n");
+		  delay(2);
+	  }
